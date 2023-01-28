5d6728e49f6a496b64c02720280f7fd98b5b725d
spencerwooo <spencer.woo@outlook.com>
2023-01-28 07:21:11 +0000

    feat(attack): add MI-FGSM attack

---
+++ torchattack/mifgsm.py
@@ -0,0 +1,108 @@
+from typing import Callable
+
+import torch
+import torch.nn as nn
+
+from torchattack.base import Attack
+
+
+class MIFGSM(Attack):
+    """The MI-FGSM (Momentum Iterative FGSM) attack.
+
+    From the paper 'Boosting Adversarial Attacks with Momentum':
+    https://arxiv.org/abs/1710.06081
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        transform: Callable[[torch.Tensor], torch.Tensor],
+        eps: float = 8 / 255,
+        steps: int = 10,
+        alpha: float | None = None,
+        decay_factor: float = 1.0,
+        clip_min: float = 0.0,
+        clip_max: float = 1.0,
+        targeted: bool = False,
+        device: torch.device | None = None,
+    ) -> None:
+        """Initialize the MI-FGSM attack.
+
+        Args:
+            model: The model to attack.
+            transform: A transform to normalize images.
+            eps: The maximum perturbation. Defaults to 8/255.
+            steps: Number of steps. Defaults to 10.
+            alpha: Step size, `eps / (steps / 2)` if None. Defaults to None.
+            decay_factor: Decay factor for the momentum term. Defaults to 1.0.
+            clip_min: Minimum value for clipping. Defaults to 0.0.
+            clip_max: Maximum value for clipping. Defaults to 1.0.
+            targeted: Targeted attack if True. Defaults to False.
+            device: Device to use for tensors. Defaults to cuda if available.
+        """
+
+        super().__init__(transform, device)
+
+        self.model = model
+        self.eps = eps
+        self.steps = steps
+        self.alpha = alpha
+        self.decay_factor = decay_factor
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+        self.targeted = targeted
+        self.lossfn = nn.CrossEntropyLoss()
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """Perform MI-FGSM on a batch of images.
+
+        Args:
+            x: A batch of images. Shape: (N, C, H, W).
+            y: A batch of labels. Shape: (N).
+
+        Returns:
+            The perturbed images if successful. Shape: (N, C, H, W).
+        """
+
+        g = torch.zeros_like(x)
+        delta = torch.zeros_like(x, requires_grad=True)
+
+        # If alpha is not given, set to eps / (steps / 2)
+        if self.alpha is None:
+            self.alpha = self.eps / (self.steps / 2)
+
+        # Perform MI-FGSM
+        for _ in range(self.steps):
+            # Compute loss
+            outs = self.model(self.transform(x + delta))
+            loss = self.lossfn(outs, y)
+
+            if self.targeted:
+                loss = -loss
+
+            # Compute gradient
+            loss.backward()
+
+            if delta.grad is None:
+                continue
+
+            # Apply momentum term
+            g = self.decay_factor * g + delta.grad / torch.mean(
+                torch.abs(delta.grad), dim=(1, 2, 3), keepdim=True
+            )
+
+            # Update delta
+            delta.data = delta.data + self.alpha * g.sign()
+            delta.data = torch.clamp(delta.data, -self.eps, self.eps)
+            delta.data = torch.clamp(x + delta.data, self.clip_min, self.clip_max) - x
+
+            # Zero out gradient
+            delta.grad.data.zero_()
+
+        return x + delta
+
+
+if __name__ == "__main__":
+    from torchattack.utils import run_attack
+
+    run_attack(MIFGSM, {"eps": 8 / 255, "steps": 10})
