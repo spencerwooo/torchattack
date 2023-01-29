b1336852ca856461944ab86215322b864feca678
spencerwooo <spencer.woo@outlook.com>
2023-01-29 09:10:38 +0000

    feat(attack): add support for DI and TI-FGSM

---
+++ torchattack/difgsm.py
@@ -0,0 +1,165 @@
+from typing import Callable
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+from torchattack.base import Attack
+
+
+class DIFGSM(Attack):
+    """The DI-FGSM (Diverse-input Iterative FGSM) attack.
+
+    From the paper 'Improving Transferability of Adversarial Examples with Input
+    Diversity' https://arxiv.org/abs/1803.06978
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        transform: Callable[[torch.Tensor], torch.Tensor],
+        eps: float = 8 / 255,
+        steps: int = 10,
+        alpha: float | None = None,
+        decay: float = 1.0,
+        resize_rate: float = 0.9,
+        diversity_prob: float = 1.0,
+        clip_min: float = 0.0,
+        clip_max: float = 1.0,
+        targeted: bool = False,
+        device: torch.device | None = None,
+    ) -> None:
+        """Initialize the DI-FGSM attack.
+
+        Args:
+            model: The model to attack.
+            transform: A transform to normalize images.
+            eps: The maximum perturbation. Defaults to 8/255.
+            steps: Number of steps. Defaults to 10.
+            alpha: Step size, `eps / (steps / 2)` if None. Defaults to None.
+            decay: Decay factor for the momentum term. Defaults to 1.0.
+            resize_rate: The resize rate. Defaults to 0.9.
+            diversity_prob: Applying input diversity with probability. Defaults to 1.0.
+            clip_min: Minimum value for clipping. Defaults to 0.0.
+            clip_max: Maximum value for clipping. Defaults to 1.0.
+            targeted: Targeted attack if True. Defaults to False.
+            device: Device to use for tensors. Defaults to cuda if available.
+        """
+
+        super().__init__(transform, device)
+
+        self.model = model
+        self.eps = eps
+        self.steps = steps
+        self.alpha = alpha
+        self.decay = decay
+        self.resize_rate = resize_rate
+        self.diversity_prob = diversity_prob
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+        self.targeted = targeted
+        self.lossfn = nn.CrossEntropyLoss()
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """Perform DI-FGSM on a batch of images.
+
+        Args:
+            x: A batch of images. Shape: (N, C, H, W).
+            y: A batch of labels. Shape: (N).
+
+        Returns:
+            The perturbed images if successful. Shape: (N, C, H, W).
+        """
+
+        g = torch.zeros_like(x)
+        delta = torch.zeros_like(x, requires_grad=True)
+
+        # If alpha is not given, set to eps / (steps / 2)
+        if self.alpha is None:
+            self.alpha = self.eps / (self.steps / 2)
+
+        # Perform DI-FGSM
+        for _ in range(self.steps):
+            # Apply input diversity to intermediate images
+            x_adv = input_diversity(x + delta, self.resize_rate, self.diversity_prob)
+
+            # Compute loss
+            outs = self.model(self.transform(x_adv))
+            loss = self.lossfn(outs, y)
+
+            if self.targeted:
+                loss = -loss
+
+            # Compute gradient
+            loss.backward()
+
+            if delta.grad is None:
+                continue
+
+            # Apply momentum term
+            g = self.decay * g + delta.grad / torch.mean(
+                torch.abs(delta.grad), dim=(1, 2, 3), keepdim=True
+            )
+
+            # Update delta
+            delta.data = delta.data + self.alpha * g.sign()
+            delta.data = torch.clamp(delta.data, -self.eps, self.eps)
+            delta.data = torch.clamp(x + delta.data, self.clip_min, self.clip_max) - x
+
+            # Zero out gradient
+            delta.grad.detach_()
+            delta.grad.zero_()
+
+        return x + delta
+
+
+def input_diversity(
+    x: torch.Tensor, resize_rate: float = 0.9, diversity_prob: float = 0.5
+) -> torch.Tensor:
+    """Apply input diversity to a batch of images.
+
+    Note:
+        Adapted from the TensorFlow implementation (cihangxie/DI-2-FGSM):
+        https://github.com/cihangxie/DI-2-FGSM/blob/10ffd9b9e94585b6a3b9d6858a9a929dc488fc02/attack.py#L153-L164
+
+    Args:
+        x: A batch of images. Shape: (N, C, H, W).
+        resize_rate: The resize rate. Defaults to 0.9.
+        diversity_prob: Applying input diversity with probability. Defaults to 0.5.
+
+    Returns:
+        The diversified batch of images. Shape: (N, C, H, W).
+    """
+
+    if torch.rand(1) < diversity_prob:
+        return x
+
+    img_size = x.shape[-1]
+    img_resize = int(img_size * resize_rate)
+
+    if resize_rate < 1:
+        img_size = img_resize
+        img_resize = x.shape[-1]
+
+    rnd = torch.randint(low=img_size, high=img_resize, size=(1,), dtype=torch.int32)
+    rescaled = f.interpolate(x, size=[rnd, rnd], mode="nearest")
+
+    h_rem = img_resize - rnd
+    w_rem = img_resize - rnd
+
+    pad_top = torch.randint(low=0, high=h_rem.item(), size=(1,), dtype=torch.int32)
+    pad_bottom = h_rem - pad_top
+    pad_left = torch.randint(low=0, high=w_rem.item(), size=(1,), dtype=torch.int32)
+    pad_right = w_rem - pad_left
+
+    pad = [pad_left.item(), pad_right.item(), pad_top.item(), pad_bottom.item()]
+    padded = f.pad(rescaled, pad=pad, mode="constant", value=0)
+
+    return padded
+
+
+if __name__ == "__main__":
+    from torchattack.utils import run_attack
+
+    cfgs = {"eps": 8 / 255, "steps": 10, "resize_rate": 0.9, "diversity_prob": 1.0}
+    run_attack(DIFGSM, kwargs=cfgs)
---
+++ torchattack/tifgsm.py
@@ -0,0 +1,143 @@
+from typing import Callable
+
+import numpy as np
+import scipy.stats as st
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+from torchattack.base import Attack
+
+
+class TIFGSM(Attack):
+    """The TI-FGSM (Translation-invariant Iterative FGSM) attack.
+
+    From the paper 'Evading Defenses to Transferable Adversarial Examples by
+    Translation-Invariant Attacks' https://arxiv.org/abs/1904.02884
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        transform: Callable[[torch.Tensor], torch.Tensor],
+        eps: float = 8 / 255,
+        steps: int = 10,
+        alpha: float | None = None,
+        decay: float = 1.0,
+        kern_len: int = 15,
+        n_sig: int = 3,
+        clip_min: float = 0.0,
+        clip_max: float = 1.0,
+        targeted: bool = False,
+        device: torch.device | None = None,
+    ) -> None:
+        """Initialize the TI-FGSM attack.
+
+        Args:
+            model: The model to attack.
+            transform: A transform to normalize images.
+            eps: The maximum perturbation. Defaults to 8/255.
+            steps: Number of steps. Defaults to 10.
+            alpha: Step size, `eps / (steps / 2)` if None. Defaults to None.
+            decay: Decay factor for the momentum term. Defaults to 1.0.
+            kern_len: Length of the kernel (should be an odd number). Defaults to 15.
+            n_sig: Radius of the gaussian kernel. Defaults to 3.
+            clip_min: Minimum value for clipping. Defaults to 0.0.
+            clip_max: Maximum value for clipping. Defaults to 1.0.
+            targeted: Targeted attack if True. Defaults to False.
+            device: Device to use for tensors. Defaults to cuda if available.
+        """
+
+        super().__init__(transform, device)
+
+        self.model = model
+        self.eps = eps
+        self.steps = steps
+        self.alpha = alpha
+        self.decay = decay
+        self.kern_len = kern_len
+        self.n_sig = n_sig
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+        self.targeted = targeted
+        self.lossfn = nn.CrossEntropyLoss()
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """Perform TI-FGSM on a batch of images.
+
+        Args:
+            x: A batch of images. Shape: (N, C, H, W).
+            y: A batch of labels. Shape: (N).
+
+        Returns:
+            The perturbed images if successful. Shape: (N, C, H, W).
+        """
+
+        g = torch.zeros_like(x)
+        delta = torch.zeros_like(x, requires_grad=True)
+
+        # Get kernel
+        kernel = self.get_kernel()
+
+        # If alpha is not given, set to eps / (steps / 2)
+        if self.alpha is None:
+            self.alpha = self.eps / (self.steps / 2)
+
+        # Perform TI-FGSM
+        for _ in range(self.steps):
+            # Compute loss
+            outs = self.model(self.transform(x + delta))
+            loss = self.lossfn(outs, y)
+
+            if self.targeted:
+                loss = -loss
+
+            # Compute gradient
+            loss.backward()
+
+            if delta.grad is None:
+                continue
+
+            # Apply kernel to gradient
+            g = f.conv2d(delta.grad, kernel, stride=1, padding="same", groups=3)
+
+            # Apply momentum term
+            g = self.decay * g + g / torch.mean(
+                torch.abs(g), dim=(1, 2, 3), keepdim=True
+            )
+
+            # Update delta
+            delta.data = delta.data + self.alpha * g.sign()
+            delta.data = torch.clamp(delta.data, -self.eps, self.eps)
+            delta.data = torch.clamp(x + delta.data, self.clip_min, self.clip_max) - x
+
+            # Zero out gradient
+            delta.grad.detach_()
+            delta.grad.zero_()
+
+        return x + delta
+
+    def get_kernel(self) -> torch.Tensor:
+        kernel = self.gkern(self.kern_len, self.n_sig).astype(np.float32)
+
+        kernel = np.expand_dims(kernel, axis=0)  # (W, H) -> (1, W, H)
+        kernel = np.repeat(kernel, 3, axis=0)  # -> (C, W, H)
+        kernel = np.expand_dims(kernel, axis=1)  # -> (C, 1, W, H)
+        return torch.from_numpy(kernel).to(self.device)
+
+    @staticmethod
+    def gkern(kern_len: int = 15, n_sig: int = 3) -> np.ndarray:
+        """Return a 2D Gaussian kernel array."""
+
+        interval = (2 * n_sig + 1.0) / kern_len
+        x = np.linspace(-n_sig - interval / 2.0, n_sig + interval / 2.0, kern_len + 1)
+        kern1d = np.diff(st.norm.cdf(x))
+        kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
+        kernel = kernel_raw / kernel_raw.sum()
+        return kernel
+
+
+if __name__ == "__main__":
+    from torchattack.utils import run_attack
+
+    run_attack(TIFGSM, {"eps": 8 / 255, "steps": 10, "kern_len": 15, "n_sig": 3})
