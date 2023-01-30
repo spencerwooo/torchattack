bd6d04d7bdd40bab4479f6deeb474f7b52f4b40a
spencerwooo <spencer.woo@outlook.com>
2023-01-30 03:56:03 +0000

    feat(attack): implement variance tuning attacks

--- torchattack/__init__.py
+++ torchattack/__init__.py
@@ -1,7 +1,21 @@
+from torchattack.difgsm import DIFGSM
 from torchattack.fgsm import FGSM
 from torchattack.mifgsm import MIFGSM
 from torchattack.nifgsm import NIFGSM
 from torchattack.pgd import PGD
 from torchattack.sinifgsm import SINIFGSM
+from torchattack.tifgsm import TIFGSM
+from torchattack.vmifgsm import VMIFGSM
+from torchattack.vnifgsm import VNIFGSM
 
-__all__ = ["FGSM", "PGD", "MIFGSM", "NIFGSM", "SINIFGSM"]
+__all__ = [
+    "DIFGSM",
+    "FGSM",
+    "MIFGSM",
+    "NIFGSM",
+    "PGD",
+    "SINIFGSM",
+    "TIFGSM",
+    "VMIFGSM",
+    "VNIFGSM",
+]
---
+++ torchattack/vmifgsm.py
@@ -0,0 +1,143 @@
+from typing import Callable
+
+import torch
+import torch.nn as nn
+
+from torchattack.base import Attack
+
+
+class VMIFGSM(Attack):
+    """The VMI-FGSM (Variance-tuned Momentum Iterative FGSM) attack.
+
+    From the paper 'Enhancing the Transferability of Adversarial Attacks through
+    Variance Tuning' https://arxiv.org/abs/2103.15571
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        transform: Callable[[torch.Tensor], torch.Tensor],
+        eps: float = 8 / 255,
+        steps: int = 10,
+        alpha: float | None = None,
+        decay: float = 1.0,
+        n: int = 5,
+        beta: float = 1.5,
+        clip_min: float = 0.0,
+        clip_max: float = 1.0,
+        targeted: bool = False,
+        device: torch.device | None = None,
+    ) -> None:
+        """Initialize the VMI-FGSM attack.
+
+        Note:
+            Key parameters are `n` and `beta`, where `n` is the number of sampled
+            examples for variance tuning and `beta` is the upper bound of the
+            neighborhood for varying the perturbation.
+
+        Args:
+            model: The model to attack.
+            transform: A transform to normalize images.
+            eps: The maximum perturbation. Defaults to 8/255.
+            steps: Number of steps. Defaults to 10.
+            alpha: Step size, `eps / (steps / 2)` if None. Defaults to None.
+            decay: Decay factor for the momentum term. Defaults to 1.0.
+            n: Number of sampled examples for variance tuning. Defaults to 5.
+            beta: The upper bound of the neighborhood. Defaults to 1.5.
+            clip_min: Minimum value for clipping. Defaults to 0.0.
+            clip_max: Maximum value for clipping. Defaults to 1.0.
+            targeted: Targeted attack if True. Defaults to False.
+            device: Device to use for tensors. Defaults to cuda if available.
+        """
+
+        super().__init__(transform, device)
+
+        self.model = model
+        self.eps = eps
+        self.steps = steps
+        self.alpha = alpha
+        self.decay = decay
+        self.n = n
+        self.beta = beta
+        self.clip_min = clip_min
+        self.clip_max = clip_max
+        self.targeted = targeted
+        self.lossfn = nn.CrossEntropyLoss()
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        """Perform VMI-FGSM on a batch of images.
+
+        Args:
+            x: A batch of images. Shape: (N, C, H, W).
+            y: A batch of labels. Shape: (N).
+
+        Returns:
+            The perturbed images if successful. Shape: (N, C, H, W).
+        """
+
+        g = torch.zeros_like(x)  # Momentum
+        v = torch.zeros_like(x)  # Gradient variance
+        delta = torch.zeros_like(x, requires_grad=True)
+
+        # If alpha is not given, set to eps / (steps / 2)
+        if self.alpha is None:
+            self.alpha = self.eps / (self.steps / 2)
+
+        # Perform VMI-FGSM
+        for _ in range(self.steps):
+            # Compute loss
+            outs = self.model(self.transform(x + delta))
+            loss = self.lossfn(outs, y)
+
+            if self.targeted:
+                loss = -loss
+
+            # Compute gradient
+            loss.backward()
+
+            if delta.grad is None:
+                continue
+
+            # Apply momentum term and variance
+            delta_grad = delta.grad
+            g = self.decay * g + (delta_grad + v) / torch.mean(
+                torch.abs(delta_grad + v), dim=(1, 2, 3), keepdim=True
+            )
+
+            # Compute gradient variance
+            gv_grad = torch.zeros_like(x)
+            for _ in range(self.n):
+                # Get neighboring samples perturbation
+                neighbors = delta.data + torch.randn_like(x).uniform_(
+                    -self.eps * self.beta, self.eps * self.beta
+                )
+                neighbors.requires_grad_()
+                neighbor_outs = self.model(self.transform(x + neighbors))
+                neighbor_loss = self.lossfn(neighbor_outs, y)
+
+                if self.targeted:
+                    neighbor_loss = -neighbor_loss
+
+                neighbor_loss.backward()
+
+                gv_grad += neighbors.grad
+
+            # Accumulate gradient variance into v
+            v = gv_grad / self.n - delta_grad
+
+            # Update delta
+            delta.data = delta.data + self.alpha * g.sign()
+            delta.data = torch.clamp(delta.data, -self.eps, self.eps)
+            delta.data = torch.clamp(x + delta.data, self.clip_min, self.clip_max) - x
+
+            # Zero out gradient
+            delta.grad.detach_()
+            delta.grad.zero_()
+
+        return x + delta
+
+
+if __name__ == "__main__":
+    from torchattack.utils import run_attack
+
+    run_attack(VMIFGSM, {"eps": 8 / 255, "steps": 10, "n": 5, "beta": 1.5})
