ba92a10ac8bcef00f1d3a8c49ddaac97da948bd6
spencerwooo <spencer.woo@outlook.com>
2024-12-17 11:34:39 +0800

    Refactor create_attack to use kwargs for additional parameters and improve test cases

--- tests/test_create_attack.py
+++ tests/test_create_attack.py
@@ -6,7 +6,7 @@ from torchattack import create_attack
 
 @pytest.mark.parametrize(
     ('attack_name', 'expected'),
-    [i for i in torchattack.GRADIENT_NON_VIT_ATTACKS.items() if i[0] != 'DR'],
+    [kv for kv in torchattack.GRADIENT_NON_VIT_ATTACKS.items() if kv[0] != 'DR'],
 )
 def test_create_non_vit_attack_same_as_imported(
     attack_name,
@@ -18,10 +18,7 @@ def test_create_non_vit_attack_same_as_imported(
     assert created_attacker == expected_attacker
 
 
-def test_create_dr_attack_same_as_imported(
-    vgg16_model,
-    data,
-):
+def test_create_dr_attack_same_as_imported(vgg16_model):
     created_attacker = create_attack('DR', vgg16_model)
     expected_attacker = torchattack.DR(vgg16_model)
     assert created_attacker == expected_attacker
@@ -51,100 +48,50 @@ def test_create_generative_attack_same_as_imported(attack_name, expected):
 
 def test_create_attack_with_eps(device, resnet50_model):
     eps = 0.3
-    attack_args = {}
     attacker = create_attack(
         attack='FGSM',
         model=resnet50_model,
         normalize=resnet50_model.normalize,
         device=device,
         eps=eps,
-        attack_args=attack_args,
     )
     assert attacker.eps == eps
 
 
-def test_create_attack_with_attack_args_eps(device, resnet50_model):
-    attack_args = {'eps': 0.1}
+def test_create_attack_with_extra_args(device, resnet50_model):
+    attack_args = {'eps': 0.1, 'steps': 40, 'alpha': 0.01, 'decay': 0.9}
     attacker = create_attack(
-        attack='FGSM',
+        attack='MIFGSM',
         model=resnet50_model,
         normalize=resnet50_model.normalize,
         device=device,
-        attack_args=attack_args,
+        **attack_args,
     )
     assert attacker.eps == attack_args['eps']
-
-
-def test_create_attack_with_both_eps_and_attack_args(device, resnet50_model):
-    eps = 0.3
-    attack_args = {'eps': 0.1}
-    with pytest.warns(
-        UserWarning,
-        match="The 'eps' value provided as an argument will overwrite the existing "
-        "'eps' value in 'attack_args'. This MAY NOT be the intended behavior.",
-    ):
-        attacker = create_attack(
-            attack='FGSM',
-            model=resnet50_model,
-            normalize=resnet50_model.normalize,
-            device=device,
-            eps=eps,
-            attack_args=attack_args,
-        )
-    assert attacker.eps == eps
-
-
-def test_create_attack_with_both_weights_and_attack_args(device):
-    weights = 'VGG19_IMAGENET'
-    attack_args = {'weights': 'VGG19_IMAGENET'}
-    with pytest.warns(
-        UserWarning,
-        match="The 'weights' value provided as an argument will "
-        "overwrite the existing 'weights' value in 'attack_args'. "
-        'This MAY NOT be the intended behavior.',
-    ):
-        attacker = create_attack(
-            attack='CDA',
-            device=device,
-            weights=weights,
-            attack_args=attack_args,
-        )
-    assert attacker.weights == weights
+    assert attacker.steps == attack_args['steps']
+    assert attacker.alpha == attack_args['alpha']
+    assert attacker.decay == attack_args['decay']
 
 
 def test_create_attack_with_invalid_eps(device, resnet50_model):
-    eps = 0.3
-    with pytest.warns(
-        UserWarning, match="argument 'eps' is invalid in DeepFool and will be ignored."
-    ):
-        attacker = create_attack(
+    with pytest.raises(TypeError):
+        create_attack(
             attack='DeepFool',
             model=resnet50_model,
             normalize=resnet50_model.normalize,
             device=device,
-            eps=eps,
+            eps=0.03,
         )
-    assert 'eps' not in attacker.__dict__
 
 
-def test_create_attack_with_weights_and_checkpoint_path(device):
+def test_create_attack_cda_with_weights(device):
     weights = 'VGG19_IMAGENET'
-    checkpoint_path = 'path/to/checkpoint'
-    attack_args = {}
-    with pytest.warns(
-        UserWarning,
-        match="argument 'weights' and 'checkpoint_path' are only used for "
-        "generative attacks, and will be ignored for 'FGSM'.",
-    ):
-        attacker = create_attack(
-            attack='FGSM',
-            device=device,
-            weights=weights,
-            checkpoint_path=checkpoint_path,
-            attack_args=attack_args,
-        )
-    assert 'weights' not in attacker.__dict__
-    assert 'checkpoint_path' not in attacker.__dict__
+    attacker = create_attack(
+        attack='CDA',
+        device=device,
+        weights=weights,
+    )
+    assert attacker.weights == weights
 
 
 def test_create_attack_with_invalid_attack_name(device, resnet50_model):
