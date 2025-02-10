d9498f9c88c5f455a15e1bdd2653832cf1aa8235
spencerwooo <spencer.woo@outlook.com>
2025-02-10 16:41:18 +0800

    Try make mypy happy about str

--- torchattack/__init__.py
+++ torchattack/__init__.py
@@ -32,7 +32,7 @@ from torchattack.vdc import VDC
 from torchattack.vmifgsm import VMIFGSM
 from torchattack.vnifgsm import VNIFGSM
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
 
 __all__ = [
     # Helper function to create an attack by its name
--- torchattack/vdc.py
+++ torchattack/vdc.py
@@ -60,6 +60,7 @@ class VDC(Attack):
         elif hasattr(model, 'model_name'):
             # If model is initialized via `torchattack.AttackModel`, the model_name
             # is automatically attached to the model during instantiation.
+            assert isinstance(str, model.model_name)
             self.hook_cfg = model.model_name
 
         self.eps = eps
