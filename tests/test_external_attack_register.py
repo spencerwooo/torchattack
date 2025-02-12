f17e6474fe25d1c45948cbf15b6d054051f467bf
spencerwooo <spencer.woo@outlook.com>
2025-02-12 17:48:21 +0800

    Add attack register tests for external attacks

---
+++ tests/test_external_attack_register.py
@@ -0,0 +1,22 @@
+from torchattack import ATTACK_REGISTRY, Attack, create_attack, register_attack
+
+
+@register_attack()
+class ExternalNewAttack(Attack):
+    def __init__(self, model, normalize, device):
+        super().__init__(model, normalize, device)
+
+    def forward(self, x):
+        return x
+
+
+def test_external_attack_registered():
+    assert 'ExternalNewAttack' in ATTACK_REGISTRY
+    assert issubclass(ATTACK_REGISTRY['ExternalNewAttack'], Attack)
+    assert ATTACK_REGISTRY['ExternalNewAttack'].attack_name == 'ExternalNewAttack'
+    assert ATTACK_REGISTRY['ExternalNewAttack'].is_common()
+
+
+def test_external_attack_can_be_created():
+    ea = create_attack('ExternalNewAttack', model=None, normalize=None, device=None)
+    assert isinstance(ea, ExternalNewAttack)
