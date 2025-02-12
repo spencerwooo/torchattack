b665d8449f533b0eec4d59e1a8b54c069471b3c0
spencerwooo <spencer.woo@outlook.com>
2025-02-12 17:18:40 +0800

    Populate attack docs with new registry api

--- docs/populate_attack_apis.py
+++ docs/populate_attack_apis.py
@@ -17,24 +17,25 @@ import os
 
 import mkdocs_gen_files
 
-import torchattack as ta
+import torchattack
+from torchattack import ATTACK_REGISTRY
 
-for attack in ta.SUPPORTED_ATTACKS:
-    filename = os.path.join('attacks', f'{attack.lower()}.md')
+for attack_name, attack_cls in ATTACK_REGISTRY:
+    filename = os.path.join('attacks', f'{attack_name.lower()}.md')
     with mkdocs_gen_files.open(filename, 'w') as f:
-        if attack in ta.GENERATIVE_ATTACKS:
+        if attack_cls.is_generative():
             # For generative attacks, we need to document
             # both the attack and its weights enum as well
-            attack_mod = getattr(ta, attack.lower())
-            weights_enum = getattr(attack_mod, f'{attack}Weights')
+            attack_mod = getattr(torchattack, attack_name.lower())
+            weights_enum = getattr(attack_mod, f'{attack_name}Weights')
             weights_doc = [f'- `{w}`\n' for w in weights_enum.__members__]
             f.write(
-                f'# {attack}\n\n'
-                f'::: torchattack.{attack}\n'
-                f'::: torchattack.{attack.lower()}.{attack}Weights\n\n'
+                f'# {attack_name}\n\n'
+                f'::: torchattack.{attack_name}\n'
+                f'::: torchattack.{attack_name.lower()}.{attack_name}Weights\n\n'
                 f'Available weights:\n\n'
                 f'{"".join(weights_doc)}\n'
             )
         else:
-            f.write(f'# {attack}\n\n::: torchattack.{attack}\n')
+            f.write(f'# {attack_name}\n\n::: torchattack.{attack_name}\n')
     mkdocs_gen_files.set_edit_path(filename, 'docs/populate_attack_apis.py')
